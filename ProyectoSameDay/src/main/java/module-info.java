dca78627346b9ccd10c78db1d39b203b38a6841d
Bautista2427 <juanfelipef.bautistao@uqvirtual.edu.co>
2025-10-16 21:48:37 -0500

    Creación Proyecto

---
+++ ProyectoSameDay/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.poo.proyectosameday {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.poo.proyectosameday to javafx.fxml;
+    exports co.edu.uniquindio.poo.proyectosameday;
+}
